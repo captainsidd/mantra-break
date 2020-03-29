8c76ae866a89d49430d6c9c3d7570790708804a3
Sid Nutulapati <sid.nutulapati@nytimes.com>
2020-03-29 13:58:56 -0400

    Updates

---
+++ precache-manifest.026f4d2f0011928d4a9629c361d92cef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "865ece0ad8119b66a8b91704bc53d32f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0565d63892f326c5a37",
+    "url": "/static/css/2.440ac8e8.chunk.css"
+  },
+  {
+    "revision": "690c442ff4ce34167e88",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a0565d63892f326c5a37",
+    "url": "/static/js/2.312b2ff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.312b2ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690c442ff4ce34167e88",
+    "url": "/static/js/main.03629933.chunk.js"
+  },
+  {
+    "revision": "ec874a6e592a82b5ecb8",
+    "url": "/static/js/runtime-main.5bd85aa4.js"
+  }
+]);
