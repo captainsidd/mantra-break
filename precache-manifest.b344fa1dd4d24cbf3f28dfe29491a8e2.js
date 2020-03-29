6e00ddf258d6eb234853239f88c5b22cb32a4f79
Sid Nutulapati <sid.nutulapati@nytimes.com>
2020-03-29 14:23:24 -0400

    Updates

---
+++ precache-manifest.b344fa1dd4d24cbf3f28dfe29491a8e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025518da0d0f7f9f2caf93fc89d97d02",
+    "url": "/corona-kirtan/index.html"
+  },
+  {
+    "revision": "a0565d63892f326c5a37",
+    "url": "/corona-kirtan/static/css/2.440ac8e8.chunk.css"
+  },
+  {
+    "revision": "a3d8873580fb4fae57ff",
+    "url": "/corona-kirtan/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a0565d63892f326c5a37",
+    "url": "/corona-kirtan/static/js/2.312b2ff9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona-kirtan/static/js/2.312b2ff9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3d8873580fb4fae57ff",
+    "url": "/corona-kirtan/static/js/main.bc228031.chunk.js"
+  },
+  {
+    "revision": "d9584d80f960b03faa51",
+    "url": "/corona-kirtan/static/js/runtime-main.badb9094.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.026f4d2f0011928d4a9629c361d92cef.js"
+  "/corona-kirtan/precache-manifest.b344fa1dd4d24cbf3f28dfe29491a8e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/corona-kirtan/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
